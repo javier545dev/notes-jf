1d9c357a8922e0cc7cc3a324b749409c666804f6
javier545dev <javierfuentes545@gmail.com>
2023-09-17 16:54:21 -0500

    creation of constants for Colors, Size and Tones

---
+++ src/constant/Constant.js
@@ -0,0 +1,41 @@
+export const Colors = {
+  white: '#FFF',
+  black: '#4D5656',
+  blackLight: '#3E3E3E',
+  dark: '#000000',
+  gray: '#D9D9D9',
+  grayLogo: '#A6ACAF',
+  grayDark: '#F6F6F6',
+  grayLight: '#F0F3F4',
+  disable: '#E7E7E7',
+  transparent: 'transparent',
+}
+
+export const Size = {
+  text_xxxs: 8,
+  text_xxs: 10,
+  text_xs: 12,
+  text_sm: 14,
+  text_md: 16,
+  text_lg: 18,
+  text_xl: 20,
+  text_xxl: 24,
+  text_xxxl: 26,
+  text_big: 28,
+}
+
+export const Tones = {
+  red: '#E74C3C',
+  greenLight: '#F4F6F6',
+  greenLight2: '#dcfce7',
+  greenDark: '#1E8449',
+  green1: '#22A459',
+  yellowLight: '#F9E79F',
+  yellow1: '#F6FFED',
+  yellowDark: '#FFB300',
+  lilaLight: '#F4ECF7',
+  lilaDark: '#633974',
+  lila1: '#F6F6F6',
+  lila2: '#F3ECF6',
+  lila3: '#802FA3',
+}
