70960ffdec160e0a2e855c7932e304ab59294d7a
javier545dev <javierfuentes545@gmail.com>
2023-09-18 10:31:42 -0500

    Update Stack Navigation

--- src/routes/Stack.js
+++ src/routes/Stack.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 // Screens
 import Home from '../pages/Home'
 import Detail from '../pages/Detail'
+import Create from '../pages/Create'
 
 const Stack = createNativeStackNavigator()
 
@@ -16,7 +17,22 @@ export default function MyStack() {
           headerShown: false,
         }}
       />
-      <Stack.Screen name="Detail" component={Detail} />
+      <Stack.Screen
+        name="Detail"
+        component={Detail}
+        options={({ route }) => ({
+          title: route.params.item.title,
+          headerStyle: {
+            backgroundColor: route.params.item.colorPriority,
+          },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        })}
+      />
+      <Stack.Screen name="Create" component={Create} />
+      <Stack.Screen name="Edit" component={Create} />
     </Stack.Navigator>
   )
 }
