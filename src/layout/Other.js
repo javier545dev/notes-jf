940af3d8c18dc365ae52642a6ecf504cb59cc9c6
javier545dev <javierfuentes545@gmail.com>
2023-09-18 10:32:38 -0500

    Create and Update Layouts

--- src/layout/Other.js
+++ src/layout/Other.js
@@ -2,8 +2,12 @@ import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Colors } from '../constant/Constant'
 
-export default function Home({ children }) {
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>
+export default function Other({ children }) {
+  return (
+    <SafeAreaView edges={['left', 'right', 'bottom']} style={styles.container}>
+      {children}
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
